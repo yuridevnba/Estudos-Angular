50fd7339535a846679fabb977ec6c9b06fe07316
Yuri12 <yuri.00000830267@unicap.br>
2023-01-09 13:50:13 -0300

    Renderizando Listas

--- src/app/renderizando-listas/renderizando-listas.component.ts
+++ src/app/renderizando-listas/renderizando-listas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Celular } from '../types/Celular';
 
 @Component({
   selector: 'app-renderizando-listas',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./renderizando-listas.component.css']
 })
 export class RenderizandoListasComponent {
-
+celulares:Celular[]=[
+  {id:1,nome:"Celular XL",descricao:"Um celular grande",esgotado:false},
+  {id:2,nome:"Celular Normal",esgotado:false},
+  {id:3,nome:"Celular Mini",descricao:"Um celular pequeno",esgotado:true}
+]
 }
